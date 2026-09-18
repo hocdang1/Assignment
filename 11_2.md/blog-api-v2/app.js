3cab516037fc02f582be671dadbccf434b96f5d7
hocdang1 <hoc.dang210210@vnuk.edu.vn>
2026-09-18 16:05:38 +0700

    Revert "Remove 11_2.md/blog-api-v2 project"
    This reverts commit f1314d98ba4fa9911007ec4b09be7513bb79f0a0.

---
+++ 11_2.md/blog-api-v2/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const createError = require('http-errors');
+
+const usersRouter = require('./routes/users');
+const commentsRouter = require('./routes/comments');
+const userApiRouter = require('./routes/userApi');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.redirect('/users'));
+app.use('/users', usersRouter);
+app.use('/comments', commentsRouter);
+app.use('/api/users', userApiRouter);
+
+app.use((req, res, next) => next(createError(404)));
+
+app.locals.fmtDate = (v) => {
+  if (!v) return '-';
+  const d = v instanceof Date ? v : new Date(v);
+  return isNaN(d) ? '-' : d.toISOString().slice(0, 10);
+};
+
+app.use((err, req, res, next) => {
+  if (res.headersSent) return next(err);
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;

e18e72395fff110eddfa807a2d3e8d608af87b03
hocdang1 <118819987+hocdang1@users.noreply.github.com>
2026-09-18 16:40:30 +0700

    Delete 11.md/blog-api directory

--- 11.md/blog-api/routes/comments.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/commentController');
-
-router.get('/', ctrl.index);            // danh sách tất cả comment
-router.get('/:id/user', ctrl.showUser); // user đã viết comment đó
-
-module.exports = router;

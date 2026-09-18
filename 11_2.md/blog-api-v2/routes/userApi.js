82ec3ee04800e3ee183d20744ba70896ce36aec2
hocdang1 <118819987+hocdang1@users.noreply.github.com>
2026-09-18 16:40:40 +0700

    Delete 11_2.md/blog-api-v2 directory

--- 11_2.md/blog-api-v2/routes/userApi.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userApi = require('../controllers/userApiController');
-
-router.post('/', userApi.create);
-router.put('/:id', userApi.update);
-router.delete('/:id', userApi.remove);
-
-module.exports = router;
